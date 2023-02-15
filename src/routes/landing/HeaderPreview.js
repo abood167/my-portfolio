3c94b805b3de758710327dd07fba414b22674d7b
leam <leam@leams-MacBook-Pro.local>
2023-02-15 21:00:29 +0400

    update

--- src/routes/landing/HeaderPreview.js
+++ src/routes/landing/HeaderPreview.js
@@ -5,10 +5,10 @@ const HeaderPreview = () => {
     <article className="header">
       <h1 className="header-font white-text h1-tag">
         Hi, I'm <br></br>
-        <em className="pink-text">Catherine</em>
-        <br></br>web developer
+        <em className="pink-text">Abdul-Rahman bin Saad</em>
+        <br></br>Software Engineer
       </h1>
-      <p className="gray-text p-tag">Front End Development</p>
+      <p className="gray-text p-tag">Software Engineer</p>
       <Link className="pink-text" to="/portfolio">
         Check my work
       </Link>
