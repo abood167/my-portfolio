dd3962314fb4516e0faac9d1175b01c53d4315d5
leam <leam@leams-MacBook-Pro.local>
2023-02-17 17:46:49 +0400

    i change about me

--- src/routes/about/AboutMe.js
+++ src/routes/about/AboutMe.js
@@ -3,16 +3,11 @@ const AboutMe = () => {
     <section>
       <h2 className="title-font pink-text h2-tag">About Me</h2>
       <p className="white-text p-tag">
-        I am a front-end web developer with 9 years of working experience in
-        fields like sales, customer support, social media, and management. I
-        combine my management expertise with technical skills and fully commit
-        to consistency, meeting deadlines, and utilizing all available resources
-        to achieve results. I enjoy creating beautiful, accessible, and
-        responsive websites using HTML, CSS, Tailwind, Sass, SCSS, Vanilla
-        JavaScript, and React. Effective use of time, problem-solving, and
-        communication skills help me show a positive attitude in complex
-        situations, and my desire to learn quickly adapts me to changing
-        situations.
+        I am a software engineer with a background in business and
+        entrepreneurship. I have experience in web development, project
+        management, marketing, and business development. I am passionate about
+        building products that solve real problems and make a positive impact on
+        the world.
       </p>
     </section>
   );
