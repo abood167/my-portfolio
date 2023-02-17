95f90dfcc5d7e7f532bf4fd465a5b3d183b8e5e0
leam <leam@leams-MacBook-Pro.local>
2023-02-17 17:58:25 +0400

    i change about me

--- src/routes/about/Stack.js
+++ src/routes/about/Stack.js
@@ -9,14 +9,8 @@ const Stack = () => {
       <ol className="stack-section ">
         <li>HTML5</li>
         <li>CSS3</li>
-        <li>Tailwind</li>
-        <li>SCSS/SASS</li>
-        <li>JavaScript ES6+</li>
-        <li>React.js</li>
-        <li>RESTful API</li>
         <li>Github</li>
         <li>Git</li>
-        <li>Figma</li>
         <li>Visual Studio Code</li>
       </ol>
     </React.Fragment>
